66523eaedcb1f1ff580de16115f7fc78fe388e73
Pablo Martínez <pabloextreme27@gmail.com>
2024-06-08 08:05:45 +0200

    0.0.9

--- app/config/router.js
+++ app/config/router.js
@@ -1,8 +1,8 @@
-import { Router } from "../../assets/js/ScopeJS.min.js?v=0.0.8";
-import { HomeController } from "../controllers/HomeController.js?v=0.0.8";
-import { ExperienceController } from "../controllers/ExperienceController.js?v=0.0.8";
-import { ProjectsController } from "../controllers/ProjectsController.js?v=0.0.8";
-import { TechnologiesController } from "../controllers/TechnologiesController.js?v=0.0.8";
+import { Router } from "../../assets/js/ScopeJS.min.js?v=0.0.9";
+import { HomeController } from "../controllers/HomeController.js?v=0.0.9";
+import { ExperienceController } from "../controllers/ExperienceController.js?v=0.0.9";
+import { ProjectsController } from "../controllers/ProjectsController.js?v=0.0.9";
+import { TechnologiesController } from "../controllers/TechnologiesController.js?v=0.0.9";
 
 export const router = Router(
   [
--- app/setup.js
+++ app/setup.js
@@ -1,6 +1,6 @@
-import { Router } from "../assets/js/ScopeJS.min.js?v=0.0.8";
-import { HTTPS } from "./config/constants.js?v=0.0.8";
-import { AppController } from "./controllers/AppController.js?v=0.0.8";
+import { Router } from "../assets/js/ScopeJS.min.js?v=0.0.9";
+import { HTTPS } from "./config/constants.js?v=0.0.9";
+import { AppController } from "./controllers/AppController.js?v=0.0.9";
 
 export const router = Router(
   [
