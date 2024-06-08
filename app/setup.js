7b266171aae31181cd86130973c6ae23fbab6da4
Pablo Martínez <pabloextreme27@gmail.com>
2024-06-08 07:36:09 +0200

    local ScopeJS

--- app/setup.js
+++ app/setup.js
@@ -1,4 +1,4 @@
-import { Router } from "https://cdn.devetty.es/ScopeJS/js";
+import { Router } from "../assets/js/ScopeJS.min.js?v=0.0.8";
 import { HTTPS } from "./config/constants.js?v=0.0.8";
 import { AppController } from "./controllers/AppController.js?v=0.0.8";
 
